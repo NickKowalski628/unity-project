c0972b6b0d4ebe0d34a129c905238ed17b2272d3
NickKowalski628 <snowred500@gmail.com>
2026-06-01 15:19:27 -0400

    Edited spelling
    added comments and spelling

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -20,7 +20,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 
         }else if (transform.position.z < lowerBound) 
         {
-            Debug.Log("Game Jover!");
+            Debug.Log("Game Over!");
             Destroy(gameObject); 
         }
         
